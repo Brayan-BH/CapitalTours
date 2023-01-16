a10e59130a0fa489b00f6ad489d9b1c1531c1f82
Brayan-BH <brayangithub007@gmail.com>
2023-01-16 00:27:53 -0500

    Main content

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@
         <div class="main-head">
             <div class="head-banner">
                 <p class="banner__text">
-                    Pariatur in dolore incididunt laboris deserunt aliquip voluptate esse anim tempor do fugiat reprehenderit eiusmod.
+                    Pariatur in dolore incididunt laboris deserunt aliquip voluptate esse anim tempor do fugiat
+                    reprehenderit eiusmod.
                 </p>
                 <p class="banner__link">
                     <span class="link__text">Obtener más información</span>
@@ -19,12 +20,23 @@
                     <div class="container-content">
                         <div class="text">
                             <h1>Capital Tours</h1>
-                            <p>Conduce Con Nosotros  o solicita tu servicio</p>
-                            <a>
-                                <button class="button-clasic" type="button" role="button">
-                                    Afiliate 
-                                </button>
-                            </a>
+                            <p>Conduce Con Nosotros ó <br> solicita tu servicio</p>
+                            <div class="text__buttons">
+                                <a>
+                                    <button class="button-1" type="button" role="button">
+                                        Afiliate
+                                    </button>
+                                </a>
+                                <a>
+                                    <button class="button-1" type="button" role="button">
+                                        Contactanos
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                        <div class="text__img">
+                            <img src="	https://www.alotaxis.com/img/imagen2.svg" alt="svg">
                         </div>
                     </div>
                 </div>
