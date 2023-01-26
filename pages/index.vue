79411ee96f6bdd3790a0ac245c22d69a69f1afe1
JoshHeafy <i2225401@continental.edu.pe>
2023-01-25 23:03:27 -0500

    Cambio de prueba

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
             <div class="head-banner">
                 <p class="banner__text">
                     Pariatur in dolore incididunt laboris deserunt aliquip voluptate esse anim tempor do fugiat
-                    reprehenderit eiusmod.
+                    reprehenderit sapeeee.
                 </p>
                 <p class="banner__link">
                     <span class="link__text">Obtener más información</span>
