2ee4b40d2588d6d01a6d0b1946f29c596c84e2c0
Brayan-BH <brayangithub007@gmail.com>
2023-01-25 23:42:02 -0500

    Prueba 4

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
                     Pariatur in dolore incididunt laboris deserunt aliquip voluptate esse anim tempor do fugiat
                     reprehenderit sapeeee.
                 </p>
+                <p>Sapeeee 2</p>
                 <p class="banner__link">
                     <span class="link__text">Obtener más información</span>
                     <span class="link__icon material-icons">navigate_next</span>
